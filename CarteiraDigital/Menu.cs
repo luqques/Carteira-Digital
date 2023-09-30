dfc5f81610b431f439de28daa5ec453c5cbca958
Lucas da Cunha <luques.cunha@gmail.com>
2023-09-30 02:14:57 -0300

    Criação da Classe Menu

---
+++ CarteiraDigital/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carteira
+{
+    internal class Menu
+    {
+        public static string MenuPrincipal()
+        {
+            Console.WriteLine("1. Carteira");
+            return Console.ReadLine();
+        }
+    }
+}
