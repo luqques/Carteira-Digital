8f913cdb3b08e6cbcbe836e1d486293a348b5e74
Lucas da Cunha <luques.cunha@gmail.com>
2023-10-03 21:41:44 -0300

    Alterado o método de cadastro de Conta na Carteira

--- CarteiraDigital/Conta.cs
+++ CarteiraDigital/Conta.cs
@@ -8,19 +8,18 @@ namespace Carteira
 {
     public class Conta
     {
-        private string Numero { get; set; }
-        public string Agencia { get; set; }
-        public string Banco { get; set; }
-        
-        private Carteira.Cliente cliente;
-
-        
-
         public static void CadastroConta()
         {
-            Console.WriteLine("Cadastre sua conta:");
+            Console.WriteLine("Cadastre sua conta na Carteira Digital:");
 
-            Console.Write("Digite o número da conta: ");            
+            Console.Write("Digite o número da Conta: ");
+            string numero = Console.ReadLine();
+
+            Console.Write("Digite o número da Agência: ");
+            int agencia = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o número do Banco: ");
+            int banco = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
